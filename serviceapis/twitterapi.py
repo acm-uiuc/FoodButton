f76a19b965f7457a3c6c8bd570e5ebbd1b98948e
VaibhavBajaj <vaibhav_bajaj2005@yahoo.com>
2017-09-06 16:59:50 -0500

    Added logger

--- serviceapis/twitterapi.py
+++ serviceapis/twitterapi.py
@@ -8,6 +8,7 @@ this license in a file with the distribution.
 
 from .serviceapi import ServiceAPI
 from datetime import datetime
+import logging
 import tweepy
 from PIL import Image
 
@@ -19,12 +20,12 @@ class TwitterAPI(ServiceAPI):
         auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
         auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
         self.api = tweepy.API(auth)
+        logging.basicConfig(filename=cfg['log_file'])
          
     def post(self, img, text):
         try:
             filename = "/tmp/food_on_table_{}.png".format(datetime.now())
             img.save(filename)
-            self.api.update_with_media(filename, status=text)
+            self.api.update_status(status=text)
         except tweepy.TweepError as tweepError:
-            print (tweepError.reason)  #TODO: Change to some sort of logger 
-                  
+            logging.error(msg="Error occured while tweeting image: " + tweepError.reason)
