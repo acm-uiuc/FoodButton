09d9a68d601ed87104ffd3fd03ca9b595ada982d
VaibhavBajaj <vbajajpersonal@gmail.com>
2017-09-18 21:49:37 -0500

    Edited comments

--- app.py
+++ app.py
@@ -21,30 +21,25 @@ def get_image(camera):
     return img
 
 def load_img_from_webcam():
-    # Camera 0 is the integrated web cam on my netbook
+    # Camera 1 is the USB Webcam
     camera_port = 1
 
-    #Number of frames to throw away while the camera adjusts to light levels
+    # Number of frames to throw away while the camera adjusts to light levels
     ramp_frames = 30
 
-    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
-    # All it needs is the index to a camera port.
+    # Initializing to port 1 (USB Webcam is on port 1)
     camera = cv2.VideoCapture(camera_port)
 
     # Ramp the camera - these frames will be discarded and are only used to allow v4l2
     # to adjust light levels, if necessary
     for i in range(ramp_frames):
         temp = get_image(camera)
-    print("Taking image...")
-    # Take the actual image we want to keep
+
+    # Take the image
     camera_capture = get_image(camera)
     filename = "/tmp/food_on_table_{}.jpg".format(datetime.now())
-    # A nice feature of the imwrite method is that it will automatically choose the
-    # correct format based on the file extension you provide. Convenient!
     cv2.imwrite(filename, camera_capture)
 
-    # You'll want to release the camera, otherwise you won't be able to create a new
-    # capture object until your script exits
     del(camera)
 
     return Image.open(filename)
